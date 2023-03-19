bcd3a50f724b38f6b5a5b30336ddce8e62491301
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-03-19 10:32:40 -0300

    listview

--- expedientes/views.py
+++ expedientes/views.py
@@ -3,7 +3,7 @@ from expedientes.models import Persona, Barrio, Expediente
 from django.core.paginator import Paginator
 from django.http import Http404
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic import CreateView, UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView
 from .forms import CreateNewPersona, CreateNewBarrio, CreateNewExpediente
 from django.urls import reverse_lazy
 
@@ -12,29 +12,18 @@ from django.urls import reverse_lazy
 def index(request):
     return render(request, 'index_expedientes.html')
 
-def persona_list(request):
-    persona = Persona.objects.all().order_by('apellido','nombre')
-    page = request.GET.get('page',1)
+class PersonaListView(ListView):
+    model = Persona
+    template_name = 'persona/persona_list.html'
 
-    try:
-        paginator = Paginator(persona, 6)
-        persona = paginator.page(page)
-    except:
-        raise Http404
-
-    return render(request, 'persona/persona_list.html', {
-        'entity': persona,
-        'paginator': paginator
-    })
-
-class PersonaCreateViews(SuccessMessageMixin, CreateView):
+class PersonaCreateView(SuccessMessageMixin, CreateView):
     model = Persona
     form_class = CreateNewPersona
     template_name = 'persona/persona_new.html'
     success_message = 'Se carga la persona correctamente'
     success_url = reverse_lazy('persona_list')
 
-class PersonaUpdateViews(SuccessMessageMixin, UpdateView):
+class PersonaUpdateView(SuccessMessageMixin, UpdateView):
     model = Persona
     form_class = CreateNewPersona
     template_name = 'persona/persona_edit.html'
@@ -47,29 +36,18 @@ class PersonaDeleteView(SuccessMessageMixin, DeleteView):
     template_name = 'persona/persona_delete.html'
     success_message = 'Se elimino la persona correctamente'
 
-def barrio_list(request):
-    barrio = Barrio.objects.all().order_by('nombre')
-    page = request.GET.get('page',1)
+class BarrioListView(ListView):
+    model = Barrio
+    template_name = 'barrio/barrio_list.html'
 
-    try:
-        paginator = Paginator(barrio, 6)
-        barrio = paginator.page(page)
-    except:
-        raise Http404
-
-    return render(request, 'barrio/barrio_list.html', {
-        'entity': barrio,
-        'paginator': paginator
-    })
-
-class BarrioCreateViews(SuccessMessageMixin, CreateView):
+class BarrioCreateView(SuccessMessageMixin, CreateView):
     model = Persona
     form_class = CreateNewBarrio
     template_name = 'barrio/barrio_new.html'
     success_message = 'Se cargo el barrio correctamente'
     success_url = reverse_lazy('barrio_list')
 
-class BarrioUpdateViews(SuccessMessageMixin, UpdateView):
+class BarrioUpdateView(SuccessMessageMixin, UpdateView):
     model = Barrio
     form_class = CreateNewBarrio
     template_name = 'barrio/barrio_edit.html'
@@ -82,30 +60,18 @@ class BarrioDeleteView(SuccessMessageMixin, DeleteView):
     template_name = 'barrio/barrio_delete.html'
     success_message = 'Se elimino el barrio correctamente'
 
+class ExpedienteListView(ListView):
+    model = Expediente
+    template_name = 'expediente/expediente_list.html'
 
-def expediente_list(request):
-    expediente = Expediente.objects.all().order_by('id')
-    page = request.GET.get('page',1)
-
-    try:
-        paginator = Paginator(expediente, 6)
-        expediente = paginator.page(page)
-    except:
-        raise Http404
-
-    return render(request, 'expediente/expediente_list.html', {
-        'entity': expediente,
-        'paginator': paginator
-    })
-
-class ExpedienteCreateViews(SuccessMessageMixin, CreateView):
+class ExpedienteCreateView(SuccessMessageMixin, CreateView):
     model = Expediente
     form_class = CreateNewExpediente
     template_name = 'expediente/expediente_new.html'
     success_message = 'Se cargo el expediente correctamente'
     success_url = reverse_lazy('expediente_list')
 
-class ExpedienteUpdateViews(SuccessMessageMixin, UpdateView):
+class ExpedienteUpdateView(SuccessMessageMixin, UpdateView):
     model = Expediente
     form_class = CreateNewExpediente
     template_name = 'expediente/expediente_edit.html'
