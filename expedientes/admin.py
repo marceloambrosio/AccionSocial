49b821643a6da87bfc38ca08b4da0a78169691a6
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-03-30 10:18:55 -0300

    usergroup

--- expedientes/admin.py
+++ expedientes/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Persona, Barrio, Expediente
 
 # Register your models here.
+
+class PersonaAdmin(admin.ModelAdmin):
+    search_fields = ('apellido','nombre'),
+    ordering = ['apellido','nombre']
+
+class BarrioAdmin(admin.ModelAdmin):
+    search_fields = ('nombre'),
+    ordering = ['nombre']
+
+class ExpedienteAdmin(admin.ModelAdmin):
+    ordering = ['numero_interno']
+
+
+admin.site.register(Persona, PersonaAdmin)
+admin.site.register(Barrio, BarrioAdmin)
+admin.site.register(Expediente, ExpedienteAdmin)
